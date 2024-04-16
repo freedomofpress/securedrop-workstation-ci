1806bbcfe9b6f0fc3e0977d77ce96c5d7642e987
Miguel Jacq <mig@mig5.net>
2024-04-17 09:53:20 +1000

    Tweaks to sd-dev scripts to add better logging, mandatory json attributes

--- sd-dev/bin/begin.py
+++ sd-dev/bin/begin.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
 import json
+import logging
 import os
 import subprocess
 import shutil
+from logging.handlers import SysLogHandler
+
 
 def run():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    handler = SysLogHandler(facility=SysLogHandler.LOG_DAEMON, address="/dev/log")
+    handler.setFormatter(logging.Formatter("sdw-ci-begin: %(message)s"))
+    logger.addHandler(handler)
+
     owner = "freedomofpress"
     repo = "securedrop-workstation"
 
@@ -17,10 +26,20 @@ def run():
     context_filepath = "/home/user/QubesIncoming/dom0/context.json"
     if os.path.exists(context_filepath):
         with open(context_filepath, "r") as context_file:
-            context = json.load(context_file)
+            try:
+                context = json.load(context_file)
+            except json.JSONDecodeError as e:
+                logger.debug(e)
+                raise SystemError(e)
     else:
-        raise SystemError("context.json file not found!")
-    commit_sha = context.get("commit")
+        e = "context.json file not found!"
+        logger.debug(e)
+        raise SystemError(e)
+    try:
+        commit_sha = context["commit"]
+    except KeyError as e:
+        logger.debug(e)
+        raise SystemError(e)
 
     workspace = f"{repo}_{commit_sha}"
 
@@ -29,25 +48,45 @@ def run():
         shutil.rmtree(f"{working_dir}/{workspace}")
 
     # Clone and checkout that relevant commit
-    subprocess.check_call(["git", "clone", f"https://github.com/{owner}/{repo}", f"{working_dir}/{workspace}"])
-    subprocess.check_call(["git", "checkout", commit_sha], cwd=f"{working_dir}/{workspace}")
+    logger.debug("Cloning the repo")
+    subprocess.check_call(
+        [
+            "git",
+            "clone",
+            f"https://github.com/{owner}/{repo}",
+            f"{working_dir}/{workspace}",
+        ]
+    )
+    subprocess.check_call(
+        ["git", "checkout", commit_sha], cwd=f"{working_dir}/{workspace}"
+    )
 
     # Copy the config files we need
     if os.path.exists(f"{working_dir}/{workspace}/files/config.json.example"):
-        shutil.copy(f"{working_dir}/{workspace}/files/config.json.example", f"{working_dir}/{workspace}/config.json")
+        shutil.copy(
+            f"{working_dir}/{workspace}/files/config.json.example",
+            f"{working_dir}/{workspace}/config.json",
+        )
 
     if os.path.exists(f"{working_dir}/{workspace}/sd-journalist.sec.example"):
-        shutil.copy(f"{working_dir}/{workspace}/sd-journalist.sec.example", f"{working_dir}/{workspace}/sd-journalist.sec")
+        shutil.copy(
+            f"{working_dir}/{workspace}/sd-journalist.sec.example",
+            f"{working_dir}/{workspace}/sd-journalist.sec",
+        )
 
     # Post pending status back to Github
-    subprocess.check_call([
-        "/usr/bin/python3",
-        "/home/user/bin/status.py",
-        "--status",
-        "pending",
-    ])
+    logger.debug("Telling Github that the build is pending")
+    subprocess.check_call(
+        [
+            "/usr/bin/python3",
+            "/home/user/bin/status.py",
+            "--status",
+            "pending",
+        ]
+    )
 
     # RPC call to trigger running the build on dom0
+    logger.debug("Telling dom0 to commence CI")
     subprocess.Popen(["qrexec-client-vm", "dom0", f"qubes.SDCIRunner+{workspace}"])
 
 
--- sd-dev/bin/status.py
+++ sd-dev/bin/status.py
@@ -2,8 +2,10 @@
 
 import argparse
 import json
+import logging
 import os
 import requests
+from logging.handlers import SysLogHandler
 
 
 def parse_args():
@@ -12,145 +14,185 @@ def parse_args():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument(
-            "--log",
-            default = "",
-            action="store",
-            help="Path to the log file",
+        "--log",
+        default="",
+        action="store",
+        help="Path to the log file",
     )
     parser.add_argument(
-            "--status",
-            action="store",
-            required=True,
-            help="Status of the test to send as a Github commit status",
+        "--status",
+        action="store",
+        required=True,
+        help="Status of the test to send as a Github commit status",
     )
     args = parser.parse_args()
     return args
 
 
-def commit_status(status, log, context):
-    """
-    Reports a Github commit status
-    """
-    if status == "error":
-        description = "There was a problem during the CI execution"
-    elif status == "failure":
-        description = "The build or test process failed"
-    elif status == "success":
-        description = "The build succeeded"
-    elif status == "pending":
-        description = "The build is queued"
-    elif status == "running":
-        description = "The build is running"
-    elif status == "canceled":
-        description = "The build was canceled by an administrator"
-    else:
-        raise SystemError(f"Unrecognized status: {status}")
+class Status:
+    def __init__(self):
+        """
+        Set up the Status class with attributes required.
+        """
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        handler = SysLogHandler(facility=SysLogHandler.LOG_DAEMON, address="/dev/log")
+        handler.setFormatter(logging.Formatter("sdw-ci-status: %(message)s"))
+        self.logger.addHandler(handler)
 
-    with open("/home/user/.sdci-ghp.txt") as f:
-        github_token = f.read().strip()
-    headers = {
+        # Read context in from file
+        context_filepath = "/home/user/QubesIncoming/dom0/context.json"
+        if os.path.exists(context_filepath):
+            with open(context_filepath, "r") as context_file:
+                # Load in the JSON
+                try:
+                    context = json.load(context_file)
+                except json.JSONDecodeError as e:
+                    self.logger.debug(e)
+                    raise SystemError(e)
+
+                # Set attributes from JSON
+                try:
+                    self.commit_message = context["message"].split("\n")[0]
+                    self.commit_sha = context["commit"]
+                    self.commit_author = context["author"]
+                    self.reason = context["reason"]
+                except KeyError as e:
+                    self.logger.debug(e)
+                    raise SystemError(e)
+        else:
+            e = "context.json file not found!"
+            self.logger.debug(e)
+            raise SystemError(e)
+
+    def commit_status(self, status, log):
+        """
+        Reports a Github commit status
+        """
+        if status == "error":
+            description = "There was a problem during the CI execution"
+        elif status == "failure":
+            description = "The build or test process failed"
+        elif status == "success":
+            description = "The build succeeded"
+        elif status == "pending":
+            description = "The build is queued"
+        elif status == "running":
+            description = "The build is running"
+        elif status == "canceled":
+            description = "The build was canceled by an administrator"
+        else:
+            raise SystemError(f"Unrecognized status: {status}")
+
+        with open("/home/user/.sdci-ghp.txt") as f:
+            github_token = f.read().strip()
+        headers = {
             "Authorization": f"Bearer {github_token}",
             "Content-Type": "application/json",
-    }
-    data = {}
-    data["context"] = "sd-ci-runner"
-    data["description"] = description
-    if status in ["error", "failure", "success"]:
-        data["target_url"] = f"https://ws-ci-runner.securedrop.org/{log}"
+        }
+        data = {}
+        data["context"] = "sd-ci-runner"
+        data["description"] = description
+        if status in ["error", "failure", "success"]:
+            data["target_url"] = f"https://ws-ci-runner.securedrop.org/{log}"
 
-    # Github expects state 'error', 'failure', 'success' or 'pending'.
-    # Override our non-standard statuses to the closest match to make the
-    # API call work.
-    if status == "canceled":
-        status = "error"
-    if status == "running":
-        status = "pending"
-    data["state"] = status
+        # Github expects state 'error', 'failure', 'success' or 'pending'.
+        # Override our non-standard statuses to the closest match to make the
+        # API call work.
+        if status == "canceled":
+            status = "error"
+        if status == "running":
+            status = "pending"
+        data["state"] = status
 
-    commit_sha = context.get("commit")
+        self.logger.debug(f"Posting commit status for {self.commit_sha} to Github")
+        requests.post(
+            f"https://api.github.com/repos/freedomofpress/securedrop-workstation/statuses/{self.commit_sha}",
+            json=data,
+            headers=headers,
+        )
 
-    requests.post(f"https://api.github.com/repos/freedomofpress/securedrop-workstation/statuses/{commit_sha}", json=data, headers=headers)
+    def notify_slack(self, status, log):
+        """
+        Notifies Slack if the build failed somehow.
+        """
+        with open("/home/user/.slack-webhook.txt", "r") as s:
+            slack_webhook_url = s.readline().strip()
 
+        commit_url = f"https://github.com/freedomofpress/securedrop-workstation/commit/{self.commit_sha}"
+        if self.reason == "nightly":
+            text = "This CI run was a nightly automated test of the HEAD commit"
+        elif self.reason == "push":
+            text = "This CI run was triggered by"
+        else:
+            text = self.reason
+        text = (
+            text
+            + f": <{commit_url}|{self.commit_sha} by {self.commit_author}: {self.commit_message}>"
+        )
 
-def notify_slack(status, log, context):
-    """
-    Notifies Slack if the build failed somehow.
-    """
-    with open("/home/user/.slack-webhook.txt", "r") as s:
-        slack_webhook_url = s.readline().strip()
+        if status == "error" or status == "failure":
+            color = "danger"
+        elif status == "success":
+            color = "good"
+        else:
+            # Don't send notifications for pending/running status
+            self.logger.debug(
+                "Not posting to Slack, the status is only that we are pending or running the build"
+            )
+            return
 
-    # Read context in from file
-    commit_message = context.get("message").split("\n")[0]
-    commit_sha = context.get("commit")
-    commit_author = context.get("author")
-    reason = context.get("reason")
+        message = {
+            "attachments": [
+                {
+                    "color": color,
+                    "pretext": f"SDW CI job {status}",
+                    "title": "Commit",
+                    "title_link": commit_url,
+                    "text": text,
+                    "fallback": text,
+                    "actions": [
+                        {
+                            "type": "button",
+                            "text": "View Log Output",
+                            "url": f"https://ws-ci-runner.securedrop.org/{log}",
+                        }
+                    ],
+                }
+            ]
+        }
 
-    commit_url = f"https://github.com/freedomofpress/securedrop-workstation/commit/{commit_sha}"
-    if reason == "nightly":
-        text = "This CI run was a nightly automated test of the HEAD commit"
-    elif reason == "push":
-        text = "This CI run was triggered by"
-    else:
-        text = reason
-    text = text + f": <{commit_url}|{commit_sha} by {commit_author}: {commit_message}>"
+        # Convert the message to JSON
+        json_message = json.dumps(message)
 
-    if status == "error" or status == "failure":
-        color = "danger"
-        pretext = "SDW CI job failed."
-    elif status == "success":
-        color = "good"
-        pretext = "SDW CI job passed."
-    else:
-        # Don't send notifications for pending/running status
-        return
-
-    message = {
-        "attachments": [
-            {
-                "color": color,
-                "pretext": pretext,
-                "title": "Commit",
-                "title_link": commit_url,
-                "text": text,
-                "fallback": text,
-                "actions": [
-                    {
-                        "type": "button",
-                        "text": "View Log Output",
-                        "url": f"https://ws-ci-runner.securedrop.org/{log}"
-                    }
-                ]
-            }
-        ]
-    }
-
-
-    # Convert the message to JSON
-    json_message = json.dumps(message)
-
-    # Attempt to post the message to Slack
-    try:
-        response = requests.post(slack_webhook_url, data=json_message, headers={'Content-Type': 'application/json'})
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        raise SystemError("HTTP error while contacting Slack")
-    except Exception as err:
-        raise SystemError(f"Other error occurred while contacting Slack: {err}")
+        # Attempt to post the message to Slack
+        try:
+            self.logger.debug("Posting build status to Slack")
+            response = requests.post(
+                slack_webhook_url,
+                data=json_message,
+                headers={"Content-Type": "application/json"},
+            )
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            e = "HTTP error while contacting Slack"
+            self.logger.debug(e)
+            raise SystemError(e)
+        except Exception as err:
+            e = f"Other error occurred while contacting Slack: {err}"
+            self.logger.debug(e)
+            raise SystemError(e)
 
 
 if __name__ == "__main__":
     # Parse args
     args = parse_args()
 
-    # Read context in from file
-    context_filepath = "/home/user/QubesIncoming/dom0/context.json"
-    if os.path.exists(context_filepath):
-        with open(context_filepath, "r") as context_file:
-            context = json.load(context_file)
-    else:
-        raise SystemError("context.json file not found!")
+    # Instantiate our class
+    status = Status()
 
     # Report Github status check
-    commit_status(args.status, args.log, context)
+    status.commit_status(args.status, args.log)
+
     # Slack notification for unsuccessful runs
-    notify_slack(args.status, args.log, context)
+    status.notify_slack(args.status, args.log)
